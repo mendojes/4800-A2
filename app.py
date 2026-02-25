6509295dc3cba192b1571f316cf50407e8c66a53
mendojes05 <mendozajessie05@gmail.com>
2026-02-25 00:30:29 -0800

    lecture 2 done

--- app.py
+++ app.py
@@ -1,25 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 data = [
     {
-        "name" : "Brazilian Skies",
-        "artist" : "Masayoshi Takanaka"
+        "name" : "Ready to Fly",
+        "artist" : "Masayoshi Takanaka",
+        "image_url" : "https://i.scdn.co/image/ab67616d0000b273ae12b15f6e150833b0772848"
     },
     {
         "name" : "Beleza Pula",
-        "artist" : "Masayoshi Takanaka"
+        "artist" : "Masayoshi Takanaka",
+        "image_url" : "https://i.scdn.co/image/ab67616d0000b273c718e0f746b0f671fd92421e"
     },
     {
         "name" : "Loretta",
-        "artist" : "Ginger Root"
+        "artist" : "Ginger Root",
+        "image_url" : "https://i.scdn.co/image/ab67616d0000b2738e9876be6bd512f0ff86de9e"
     },
     {
         "name" : "Off the Wall",
-        "artist" : "Michael Jackson"
+        "artist" : "Michael Jackson",
+        "image_url" : "https://i.scdn.co/image/ab67616d0000b27344e53f6377a1e99e13779af9"
+    },
+    {
+        "name": "Odo",
+        "artist": "Ado",
+        "image_url" : "https://i.scdn.co/image/ab67616d0000b2730f7bef8299e3738350d6846e"
+    },
+    {
+        "name": "Show",
+        "artist": "Ado",
+        "image_url": "https://i.scdn.co/image/ab67616d0000b273e204aafb5c393179c77c5253"
     },
 ]
+
+@app.route('/')
+def start_index():
+    return render_template("index.html")
 
 
 @app.route('/welcome')
